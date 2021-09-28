bfd8b91faf6fbf210deafece1476410b3b51e881
Jhilik Maji <jhilik.maji98@gmail.com>
2021-09-28 18:38:34 +0530

    Update multiple.js

--- multiple.js
+++ multiple.js
@@ -9,36 +9,37 @@ const submitBtn=document.querySelector(".submit");
 const progressText=document.querySelectorAll(".step p");
 const progressCheck=document.querySelectorAll(".step .check");
 const bullet=document.querySelectorAll(".step .bullet");
-let current=1;
+let current=0;
+
 firstNextBtn.addEventListener("click", function(){
     slidePage.style.marginLeft="-25%";
-    bullet[current-1].classList.add("active");
-    progressText[current-1].classList.add("active");
-    progressCheck[current-1].classList.add("active");
+    bullet[current].classList.add("active");
+    progressText[current].classList.add("active");
+    progressCheck[current].classList.add("active");
     current+=1;
 });
 nextBtnSec.addEventListener("click", function(){
     slidePage.style.marginLeft="-50%";
-    bullet[current-1].classList.add("active");
-    progressText[current-1].classList.add("active");
+    bullet[current].classList.add("active");
+    progressText[current].classList.add("active");
 
-    progressCheck[current-1].classList.add("active");
+    progressCheck[current].classList.add("active");
     current+=1;
 });
 nextBtnThird.addEventListener("click", function(){
     slidePage.style.marginLeft="-75%";
-    bullet[current-1].classList.add("active");
-    progressText[current-1].classList.add("active");
+    bullet[current].classList.add("active");
+    progressText[current].classList.add("active");
 
-    progressCheck[current-1].classList.add("active");
+    progressCheck[current].classList.add("active");
     current+=1;
 });
 submitBtn.addEventListener("click", function(){
     
-    bullet[current-1].classList.add("active");
-    progressText[current-1].classList.add("active");
+    bullet[current].classList.add("active");
+    progressText[current].classList.add("active");
 
-    progressCheck[current-1].classList.add("active");
+    progressCheck[current].classList.add("active");
     current+=1;
     setTimeout(function(){
         alert("You're Successfully Registered");
@@ -48,28 +49,28 @@ submitBtn.addEventListener("click", function(){
 prevBtnSec.addEventListener("click", function(){
     slidePage.style.marginLeft="0%";
     
-    bullet[current-2].classList.remove("active");
-    progressText[current-2].classList.remove("active");
+    bullet[current-1].classList.remove("active");
+    progressText[current-1].classList.remove("active");
 
-    progressCheck[current-2].classList.remove("active");
+    progressCheck[current-1].classList.remove("active");
     current-=1;
 });
 prevBtnThird.addEventListener("click", function(){
     slidePage.style.marginLeft="-25%";
     
-    bullet[current-2].classList.remove("active");
-    progressText[current-2].classList.remove("active");
+    bullet[current-1].classList.remove("active");
+    progressText[current-1].classList.remove("active");
 
-    progressCheck[current-2].classList.remove("active");
+    progressCheck[current-1].classList.remove("active");
     current-=1;
 });
 prevBtnFourth.addEventListener("click", function(){
     slidePage.style.marginLeft="-50%";
     
-    bullet[current-2].classList.remove("active");
-    progressText[current-2].classList.remove("active");
+    bullet[current-1].classList.remove("active");
+    progressText[current-1].classList.remove("active");
 
-    progressCheck[current-2].classList.remove("active");
+    progressCheck[current-1].classList.remove("active");
     current-=1;
 });
 
