824866ccc44fd6d96cff74dea1dc13f3b6fb394f
Jhilik Maji <jhilik.maji98@gmail.com>
2021-09-30 11:08:14 +0530

    Update multiple.js

--- multiple.js
+++ multiple.js
@@ -1,15 +1,15 @@
-const slidePage=document.querySelector(".slidepage");
-const firstNextBtn=document.querySelector(".nextBtn");
-const prevBtnSec=document.querySelector(".prev-1 ");
-const nextBtnSec=document.querySelector(".next-1");
-const prevBtnThird=document.querySelector(".prev-2 ");
-const nextBtnThird=document.querySelector(".next-2");
-const prevBtnFourth=document.querySelector(".prev-3 ");
-const submitBtn=document.querySelector(".submit");
-const progressText=document.querySelectorAll(".step p");
-const progressCheck=document.querySelectorAll(".step .check");
-const bullet=document.querySelectorAll(".step .bullet");
-let current=0;
+var slidePage=document.querySelector(".slidepage");
+var firstNextBtn=document.querySelector(".nextBtn");
+var prevBtnSec=document.querySelector(".prev-1 ");
+var nextBtnSec=document.querySelector(".next-1");
+var prevBtnThird=document.querySelector(".prev-2 ");
+var nextBtnThird=document.querySelector(".next-2");
+var prevBtnFourth=document.querySelector(".prev-3 ");
+var submitBtn=document.querySelector(".submit");
+var progressText=document.querySelectorAll(".step p");
+var progressCheck=document.querySelectorAll(".step .check");
+var bullet=document.querySelectorAll(".step .bullet");
+var current=0;
 
 firstNextBtn.addEventListener("click", function(){
     slidePage.style.marginLeft="-25%";
